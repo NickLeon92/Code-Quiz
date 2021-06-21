50235b8590ae8aed0403e86ab27928a234822c7c
Nick Leon <nicolas7@vt.edu>
2021-06-20 23:29:44 -0400

    added randomized question order

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ var questionarray = ["1what does html stand for?", "2is joe biden a fascist?", "
 var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
 var choices = document.querySelectorAll("li")
 document.getElementById("list").style.display = "none"
-document.getElementById("next").style.display = "none"
 document.getElementById("results").style.display = "none"
 var question = document.getElementById("question")
 let n = Math.floor(Math.random() * questionarray.length);
 
-playgame.addEventListener("click", function() {
+ 
+playgame.addEventListener("click", play)
+function play() { 
     document.getElementById("playgame").style.display = "none";
+    console.log(questionarray)
     console.log(answers[n])
     chosenq = questionarray[n];
     answers[n] = answers[n].sort(() => Math.random() - 0.5);
@@ -19,67 +21,104 @@ playgame.addEventListener("click", function() {
     document.getElementById("third").textContent = answers[n][2];
     document.getElementById("fourth").textContent = answers[n][3];
     question.textContent = chosenq;
-})
+    check = true;
+}
 
 // var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
 
-first.addEventListener("click", function() {
-    var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
-    let firstchoice = document.getElementById("first")
-    console.log(firstchoice.textContent)
-    console.log(answers[n][0])
-    if (firstchoice.textContent  == answers[n][0]){
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "good job"
+let check = true
+
+first.addEventListener("click", choice1)
+function choice1() {
+    if (check) {
+        var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
+        let firstchoice = document.getElementById("first")
+        console.log(firstchoice.textContent)
+        console.log(answers[n][0])
+        if (firstchoice.textContent == answers[n][0]) {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "good job"
+        }
+        else {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "you suck"
+        }
+    } return check = false;
+}
+
+second.addEventListener("click", choice2)
+function choice2() {
+    if (check) {
+        var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
+        let firstchoice = document.getElementById("second")
+        console.log(firstchoice.textContent)
+        console.log(answers[n][0])
+        if (firstchoice.textContent == answers[n][0]) {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "good job"
+        }
+        else {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "you suck"
+        }
+    } return check = false;
+}
+
+third.addEventListener("click", choice3)
+function choice3() {
+    if (check) {
+        var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
+        let firstchoice = document.getElementById("third")
+        console.log(firstchoice.textContent)
+        console.log(answers[n][0])
+        if (firstchoice.textContent == answers[n][0]) {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "good job"
+        }
+        else {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "you suck"
+        }
+    } return check = false;
+}
+
+fourth.addEventListener("click", choice4)
+function choice4() {
+    if (check) {
+        var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
+        let firstchoice = document.getElementById("fourth")
+        console.log(firstchoice.textContent)
+        console.log(answers[n][0])
+        if (firstchoice.textContent == answers[n][0]) {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "good job"
+        }
+        else {
+            document.getElementById("results").style.display = "block"
+            document.getElementById("wl").textContent = "you suck"
+        }
+    } return check = false;
+}
+
+next.addEventListener("click", nextquestion)
+function nextquestion() {
+    // var questionarray = ["1what does html stand for?", "2is joe biden a fascist?", "3wanna bomb the capitol?", "4I am sad"]; 
+    document.getElementById("results").style.display = "none"
+    console.log(questionarray)
+    console.log(n)
+    questionarray.splice(n,1)
+    console.log(questionarray)
+    if (questionarray.length>2){
+    n = Math.floor(Math.random() * questionarray.length);
+    }
+    else if(questionarray.length=2){
+        n=0
+    }
+    else if(questionarray.length=1){
+        n=0
     }
     else{
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "you suck"
-    }
-})
 
-second.addEventListener("click", function() {
-    var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
-    let firstchoice = document.getElementById("second")
-    console.log(firstchoice.textContent)
-    console.log(answers[n][0])
-    if (firstchoice.textContent  == answers[n][0]){
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "good job"
     }
-    else{
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "you suck"
-    }
-})
-
-third.addEventListener("click", function() {
-    var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
-    let firstchoice = document.getElementById("third")
-    console.log(firstchoice.textContent)
-    console.log(answers[n][0])
-    if (firstchoice.textContent  == answers[n][0]){
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "good job"
-    }
-    else{
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "you suck"
-    }
-})
-
-fourth.addEventListener("click", function() {
-    var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
-    let firstchoice = document.getElementById("fourth")
-    console.log(firstchoice.textContent)
-    console.log(answers[n][0])
-    if (firstchoice.textContent  == answers[n][0]){
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "good job"
-    }
-    else{
-        document.getElementById("results").style.display = "block"
-        document.getElementById("wl").textContent = "you suck"
-    }
-}) 
-
+    play()
+ }
