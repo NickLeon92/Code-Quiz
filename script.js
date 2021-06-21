e7c2424e2eca8e00153d5eef6f6d0c8bba0f4b78
Nick Leon <nicolas7@vt.edu>
2021-06-21 12:44:27 -0400

    remove next button necessity, add scoretracker for each game played

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// function main() {
+
 var questionarray = ["1what does html stand for?", "2is joe biden a fascist?", "3wanna bomb the capitol?", "4I am sad"];
 var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
 var choices = document.querySelectorAll("li")
@@ -5,7 +7,8 @@ document.getElementById("list").style.display = "none"
 document.getElementById("results").style.display = "none"
 var question = document.getElementById("question")
 let n = Math.floor(Math.random() * questionarray.length);
-let newgame=false
+let score =0;
+let scoresignal = false;
 let i=0;
  
 playgame.addEventListener("click", play)
@@ -27,7 +30,7 @@ function play() {
     document.getElementById("third").textContent = answers[n][2];
     document.getElementById("fourth").textContent = answers[n][3];
     question.textContent = chosenq;
-    check = true;
+    scoresignal = false;
 }
 
 // var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
@@ -36,7 +39,6 @@ let check = true
 
 first.addEventListener("click", choice1)
 function choice1() {
-    if (check) {
         var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
         let firstchoice = document.getElementById("first")
         console.log(firstchoice.textContent)
@@ -44,17 +46,18 @@ function choice1() {
         if (firstchoice.textContent == answers[n][0]) {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "good job"
+            scoresignal = true;
         }
         else {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "you suck"
         }
-    } return check = false;
-}
+        nextquestion()
+    } 
+
 
 second.addEventListener("click", choice2)
 function choice2() {
-    if (check) {
         var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
         let firstchoice = document.getElementById("second")
         console.log(firstchoice.textContent)
@@ -62,17 +65,17 @@ function choice2() {
         if (firstchoice.textContent == answers[n][0]) {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "good job"
+            scoresignal = true;
         }
         else {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "you suck"
         }
-    } return check = false;
-}
+        nextquestion()
+    }
 
 third.addEventListener("click", choice3)
 function choice3() {
-    if (check) {
         var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
         let firstchoice = document.getElementById("third")
         console.log(firstchoice.textContent)
@@ -80,17 +83,18 @@ function choice3() {
         if (firstchoice.textContent == answers[n][0]) {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "good job"
+            scoresignal = true;
         }
         else {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "you suck"
         }
-    } return check = false;
-}
+        nextquestion();
+    } 
+
 
 fourth.addEventListener("click", choice4)
 function choice4() {
-    if (check) {
         var answers = [["1right", "wrong", "wrong", "wrong"], ["2right", "wrong", "wrong", "wrong"], ["3right", "wrong", "wrong", "wrong"], ["4right", "wrong", "wrong", "wrong"]];
         let firstchoice = document.getElementById("fourth")
         console.log(firstchoice.textContent)
@@ -98,32 +102,41 @@ function choice4() {
         if (firstchoice.textContent == answers[n][0]) {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "good job"
+            scoresignal = true;
         }
         else {
             document.getElementById("results").style.display = "block"
             document.getElementById("wl").textContent = "you suck"
         }
-    } return check = false;
-}
+        nextquestion();
+    } 
 
-next.addEventListener("click", nextquestion)
+
 function nextquestion() {
     // var questionarray = ["1what does html stand for?", "2is joe biden a fascist?", "3wanna bomb the capitol?", "4I am sad"]; 
-    document.getElementById("results").style.display = "none"
+    // document.getElementById("results").style.display = "none"
     console.log(i)
     i++
+    if(scoresignal){
+        score++
+    }
     if(i==4){
-        i=0
-
+        i=0;
+        return endgame()
     }
     console.log(i)
     play()
 }
 
-function Endgame(){
+function endgame(){
     question.textContent="You're done bitch"
     document.getElementById("list").style.display = "none"
+    window.alert(score)
+    score = 0;
+    play();
+
 }
+
 
 
 
